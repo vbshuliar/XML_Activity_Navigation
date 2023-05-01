496283b0ddb1db94c3deecb96fb22c0decd72024
vbshuliar <vbshuliar@gmail.com>
2023-05-01 15:11:28 +0100

    feat: companion object implemented

--- app/src/main/java/com/example/littlelemoningredients/IngridientsActivity.kt
+++ app/src/main/java/com/example/littlelemoningredients/IngridientsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.littlelemoningredients
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +13,17 @@ class IngridientsActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.ingredients_list).text =
             "<Your solution here>"
+    }
+
+    companion object {
+        val EXTRA_DISH_NAME_KEY = "DishName"
+        fun start(context: Context, dishName: String) {
+            var intent =
+                Intent(context, IngridientsActivity::class.java)
+            intent.putExtra(EXTRA_DISH_NAME_KEY, dishName).apply {
+                putExtra(EXTRA_DISH_NAME_KEY, dishName)
+            }
+            context.startActivity(intent)
+        }
     }
 }
